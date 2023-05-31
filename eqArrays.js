45a301da322719459c5b03eb6b220d5375c7db8e
ychento <132594955+ychento@users.noreply.github.com>
2023-05-31 09:38:32 -0400

    Create eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// compare array
+const eqArrays = function(array1, array2){
+  for (let i=0; i < array1.length; i++) {
+    if (array1[i]!== array2[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
