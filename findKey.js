d7fd58b3336df1c4a939ce7b922a6b1357da7e3c
Amber Chen <amberchen@Ambers-MacBook-Air-6.local>
2023-06-01 22:43:30 -0400

    add findKey.js

---
+++ findKey.js
@@ -0,0 +1,20 @@
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const restaurantRatings = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+const result = findKey(restaurantRatings, x => x.stars === 2);
+console.log(result);
