abb5bf8a2ff8a13db1b094164e5ba7e5bffdbdba
ychento <your@email.comyiyaochen@gmail.com>
2023-06-06 13:42:56 +0000

    typo change

--- test/tailTest.js
+++ test/tailTest.js
@@ -9,7 +9,7 @@
 const assert = require('chai').assert;
 const tail   = require('../tail');
 
-describe("#htail", () => {
+describe("#tail", () => {
   it("returns [6, 7] for [5, 6, 7]", () => {
     assert.deepEqual(tail([5, 6, 7]), [6, 7]);
   });
