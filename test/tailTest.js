146c4607e0964528b0b291a289d89e40bf2e56b2
ychento <your@email.comyiyaochen@gmail.com>
2023-06-06 02:00:20 +0000

    refactor tailTest.js with mocha & chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,22 @@
-const eqArrays = require('../eqArrays');
-const tail = require('../tail');
+// const eqArrays = require('../eqArrays');
+// const tail = require('../tail');
 
 
-console.log(eqArrays(tail([5,6,7]), [6,7]));
-console.log(eqArrays(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]));
+// console.log(eqArrays(tail([5,6,7]), [6,7]));
+// console.log(eqArrays(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]));
+
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#htail", () => {
+  it("returns [6, 7] for [5, 6, 7]", () => {
+    assert.deepEqual(tail([5, 6, 7]), [6, 7]);
+  });
+
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]); 
+  });
+
+  
+});
