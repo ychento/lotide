1db8de216495dfe59b83ea8f6acd700e41c1473f
ychento <your@email.comyiyaochen@gmail.com>
2023-06-06 13:57:54 +0000

    write index.js with es6 shorthand

---
+++ index.js
@@ -0,0 +1,20 @@
+// index.js
+// const head   = require('./head');
+// const tail   = require('./tail');
+// const middle = require('./middle');
+
+// module.exports = {
+//   head:   head,
+//   tail:   tail,
+//   middle: middle
+// };
+
+import head from './head';
+import tail from './tail';
+import middle from './middle';
+
+export default {
+  head,
+  tail,
+  middle
+};
