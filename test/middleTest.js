53493e3bb8b5ef0e496f069bc5ddb631e175d58d
ychento <your@email.comyiyaochen@gmail.com>
2023-06-06 13:47:01 +0000

    use mocha to test middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,18 @@
 const middle = require("../middle")
-const assertArraysEqual = require("../assertArraysEqual")
-
-// TEST CODE
-const numbers = [1, 2, 3, 4, 5, 6];
-console.log((middle(numbers)));
+const assert = require('chai').assert;
 
 
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]));
+// // TEST CODE
+// const numbers = [1, 2, 3, 4, 5, 6];
+// console.log((middle(numbers)));
+
+
+// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]));
+
+
+describe("#middle", () => {
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  
+});
